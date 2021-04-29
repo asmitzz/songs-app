8ff6f9ccf4928878d8140402b91d9effc7bba613
Asmit shrivastava <asmitshrivastava8@gmail.com>
2021-04-29 23:51:55 +0530

    addded final logo

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,13 @@
 import {NavLink} from "react-router-dom";
-import Logo from "../assets/logo.png"
+import Logo from "../assets/logo1.png"
 
 const Header = () => {
     return (
         <div className="header">
             <NavLink className="header__link logo__name" to="/">
-                <img src={Logo} width="100px" height="50px" alt="logo"/>
+                <div className="logo">
+                  <img src={Logo} width="100px" className="logo-img" height="42px" alt="logo" />
+                </div>
                 HotMusic
             </NavLink>
         </div>
